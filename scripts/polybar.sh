92018773886e993d950aefb8479117a333de4d42
FiBe4000 <fibeube@gmail.com>
2020-07-18 14:31:15 +0200

    Prepare for focus mode

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/bash
 
+# Get status of focus mode
+focus_mode=`cat /tmp/bspwmfocusmode.dat`
+
 # Terminate already running bar instances
 killall -q polybar
 
 # wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+if [ "$focus_mode" = false ]; then
+  polybar main &
+else
+  polybar focused_main &
+fi
 
 mode=`autorandr --current`
 echo $mode
 
 if [[ $mode == "docked" ]] || [[ $mode == "docked_home" ]] || [[ $mode == "docked_home_freesync" ]]; then
-  polybar second &
-  polybar third &
+  if [ "$focus_mode" = false ]; then
+    polybar second &
+    polybar third &
+  else
+    polybar focused_second &
+    polybar focused_third &
+  fi
 fi
 
 if [[ $mode == "docked_home_laptop_closed" ]]; then
-  polybar second &
+  if [ "$focus_mode" = false ]; then
+    polybar second &
+  else
+    polybar focused_second &
+  fi
 fi
